a36ad536306e64ce27c05662cee722a99da24f43
fish <fish404hsif@gmail.com>
2023-06-16 12:27:41 +0800

    add 8.9.2 MadLibs

---
+++ Chapter 9 Files and File Paths/MadLibs/MadLibs.py
@@ -0,0 +1,22 @@
+from replace import replaceAwithB
+
+templateFile = open('teplate.txt', 'r') 
+templateContent = templateFile.read()
+templateFile.close()
+
+print("Enter an adjective:")
+templateContent = replaceAwithB(templateContent, "ADJECTIVE", input())
+print("Enter a noun:")
+templateContent = replaceAwithB(templateContent, "NOUN", input())
+print("Enter a verb:")
+templateContent = replaceAwithB(templateContent, "VERB", input())
+print("Enter a noun:")
+templateContent = replaceAwithB(templateContent, "NOUN", input())
+
+print(templateContent)
+
+resultFile = open('result.txt', 'w')
+resultFile.write(templateContent)
+resultFile.close()
+
+
